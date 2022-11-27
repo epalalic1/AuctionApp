903398183b8b49a648d0693b632a1130ceeddecd
Emina <epalalic1@etf.unsa.ba>
2022-11-27 16:53:57 +0100

    Formatting changes

--- Frontend/src/app/core/components/notification/notification.component.ts
+++ Frontend/src/app/core/components/notification/notification.component.ts
@@ -7,19 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
 
-  constructor(private servis:NotificationService) { }
-  message = ""
   @Input()
   value!:number;
 
-
+  constructor() { }
+  
   ngOnInit(): void {
-    
-    /*if (this.value == 1 ){
-      this.message = "Ponuda je veca";
-    }
-    else if (this.value == 0) {
-      this.message = "Ponuda je manja";
-    }*/
   }
 }
