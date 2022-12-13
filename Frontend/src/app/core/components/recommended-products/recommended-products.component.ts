7f1e362318f731ee112e1cae589155a030d4a47b
Emina <epalalic1@etf.unsa.ba>
2022-12-13 11:54:30 +0100

    Make the requested changes

--- Frontend/src/app/core/components/recommended-products/recommended-products.component.ts
+++ Frontend/src/app/core/components/recommended-products/recommended-products.component.ts
@@ -31,29 +31,49 @@ export class RecommendedProductsComponent implements OnInit {
   ngOnInit(): void {
     /*For now we are using this user but after implementation of registration we are going to use logged in users*/
     this.user = new User(1, "user", "user", "user", "user", "user", "user", "user", 1);
-    this.apiService.getAllBids().subscribe((rez) => {
-      this.bids = <Bid[]>JSON.parse(JSON.stringify(rez));
-      this.apiService.getAllProducts().subscribe((rez) => {
-        this.products = <Product[]>JSON.parse(JSON.stringify(rez));
-        this.products.map((rez) => {
-          if (rez.userId == this.user.id) {
-            this.listOfCategories.push(rez.categoryId);
-            this.listOfSubcategories.push(rez.subcategoryId)
-          }
-        })
-        this.bids.map((rez) => {
-          if (rez.userId == this.user.id) {
-            this.listOfProductsFromBidding.push(rez.productId);
-          }
-        })
-        this.products.map((rez) => {
-          if (this.listOfProductsFromBidding.includes(rez.id)) {
-            this.listOfCategories.push(rez.categoryId);
-            this.listOfSubcategories.push(rez.subcategoryId);
-          }
-        })
-        this.recommendedProducts = this.products.filter((rez) => rez.userId != this.user.id && (this.listOfCategories.includes(rez.categoryId) || this.listOfSubcategories.includes(rez.subcategoryId)))
+    this.apiService.getAllBids().subscribe((bidResponse) => {
+      this.bids = <Bid[]>JSON.parse(JSON.stringify(bidResponse));
+      this.apiService.getAllProducts().subscribe((productResponse) => {
+        this.products = <Product[]>JSON.parse(JSON.stringify(productResponse));
+        this.findSimilarProductsFromSelling(this.products);
+        this.findSimilarProductsFromBidding(this.bids, this.products);
+        this.recommendedProducts = this.products.filter((item) => item.userId != this.user.id &&
+          (this.listOfCategories.includes(item.categoryId) || this.listOfSubcategories.includes(item.subcategoryId)))
       })
+    })
+  }
+
+  /**
+   * A method that finds the categories and subcategories to which the products sold by the user belong
+   * @param products list of all products that we map to find categories and subcategories
+   */
+
+  findSimilarProductsFromSelling(products: Product[]) {
+    products.map((item) => {
+      if (item.userId == this.user.id) {
+        this.listOfCategories.push(item.categoryId);
+        this.listOfSubcategories.push(item.subcategoryId)
+      }
+    })
+  }
+
+  /**
+   * A method that finds categories and subcategories of products on which the user  left an bid
+   * @param bids list of all bids in database
+   * @param products list of all products that we map to find categories and subcategories
+   */
+
+  findSimilarProductsFromBidding(bids: Bid[], products: Product[]) {
+    bids.map((item) => {
+      if (item.userId == this.user.id) {
+        this.listOfProductsFromBidding.push(item.productId);
+      }
+    })
+    products.map((item) => {
+      if (this.listOfProductsFromBidding.includes(item.id)) {
+        this.listOfCategories.push(item.categoryId);
+        this.listOfSubcategories.push(item.subcategoryId);
+      }
     })
   }
 }
