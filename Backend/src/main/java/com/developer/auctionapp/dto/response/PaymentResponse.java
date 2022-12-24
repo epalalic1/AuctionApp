b2236790764bf220f3079dcf5bdef37f1941358e
Emina <epalalic1@etf.unsa.ba>
2022-12-24 14:13:52 +0100

    Add frontend part for payment

--- Backend/src/main/java/com/developer/auctionapp/dto/response/PaymentResponse.java
+++ Backend/src/main/java/com/developer/auctionapp/dto/response/PaymentResponse.java
@@ -7,9 +7,10 @@ package com.developer.auctionapp.dto.response;
  */
 
 public class PaymentResponse {
+
     private final String clientSecret;
 
-    public PaymentResponse(final String clientSecret) {
+    public PaymentResponse(String clientSecret) {
         this.clientSecret = clientSecret;
     }
 
@@ -21,5 +22,3 @@ public class PaymentResponse {
         return clientSecret;
     }
 }
-
-
