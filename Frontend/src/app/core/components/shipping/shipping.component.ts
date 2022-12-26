e46c567ed7619d347c99ccd2ddcd897f51ab6c2e
Emina <epalalic1@etf.unsa.ba>
2022-12-26 14:54:08 +0100

    Make all three tabs when adding products

--- Frontend/src/app/core/components/shipping/shipping.component.ts
+++ Frontend/src/app/core/components/shipping/shipping.component.ts
@@ -17,7 +17,6 @@ export class ShippingComponent implements OnInit {
 
   clicked = false;
 
-<<<<<<< HEAD
   model: any = {};
 
   validateDate !: number;
@@ -29,11 +28,6 @@ export class ShippingComponent implements OnInit {
   ngOnInit(): void {
     let photoName = this.addItemComponent.model.photo as string;
     photoName = photoName.substring(12, photoName.length);
-=======
-  constructor(private parentRef: AddItemComponent, private componentFactoryResolver: ComponentFactoryResolver) { }
-
-  ngOnInit(): void {
->>>>>>> ba34a843 (Make all three tabs when adding products)
   }
 
   /**
@@ -42,11 +36,7 @@ export class ShippingComponent implements OnInit {
  */
 
   remove_me() {
-<<<<<<< HEAD
     this.addItemComponent.removeComponent();
-=======
-    this.parentRef.removeComponent();
->>>>>>> ba34a843 (Make all three tabs when adding products)
   }
 
   /**
