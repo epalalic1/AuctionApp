7f204a12b33eefa1deeae12cbb7ad2eab3cc3b64
Emina <epalalic1@etf.unsa.ba>
2022-12-26 14:54:08 +0100

    Make all three tabs when adding products

--- Frontend/src/app/core/components/add-item/add-item.component.ts
+++ Frontend/src/app/core/components/add-item/add-item.component.ts
@@ -57,7 +57,6 @@ export class AddItemComponent implements OnInit {
     const component = this.container.createComponent(componentFactory);
     this.components.push(component);
     this.clicked = true;
-    console.log(this.model.name + "  " + this.model.description + "*******");
   }
 
   /**
@@ -69,6 +68,7 @@ export class AddItemComponent implements OnInit {
     this.components.splice(0, 1);
     this.clicked = false;
   }
+
   /**
    * The method we use to store file that is uploaded
    * @param files list of uploaded
