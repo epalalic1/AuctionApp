99efc8bc654244f91fb2c604e0049c23d66c59e6
Emina <epalalic1@etf.unsa.ba>
2022-12-26 14:54:08 +0100

    Make all three tabs when adding products

--- Frontend/src/app/core/components/add-item/add-item.component.ts
+++ Frontend/src/app/core/components/add-item/add-item.component.ts
@@ -14,6 +14,7 @@ import { ShippingComponent } from '../shipping/shipping.component';
 export class AddItemComponent implements OnInit {
 
   components: ComponentRef<any>[] = [];
+
   listOfNameCategories: string[] = [];
 
   listOfNameSubcategories: string[] = [];
