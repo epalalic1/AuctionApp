5bdf5fde241820745abe01140a7b1a9798ca6b12
Emina <epalalic1@etf.unsa.ba>
2022-12-26 14:54:08 +0100

    Make all three tabs when adding products

--- Backend/src/main/java/com/developer/auctionapp/service/impl/InitializeServiceImpl.java
+++ Backend/src/main/java/com/developer/auctionapp/service/impl/InitializeServiceImpl.java
@@ -27,6 +27,7 @@ public class InitializeServiceImpl implements InitializeService {
 
     private final RoleRepository roleRepository;
 
+
     public InitializeServiceImpl(
             final CategoryRepository categoryRepository,
             final SubcategoryRepository subcategoryRepository,
@@ -41,9 +42,6 @@ public class InitializeServiceImpl implements InitializeService {
     private String initializeSubcategory = "";
 
     private String initializeRole = "";
-
-
-    private String initializeAddress = "";
 
 
     /**
--- Frontend/src/app/core/components/add-item/add-item.component.ts
+++ Frontend/src/app/core/components/add-item/add-item.component.ts
@@ -15,6 +15,9 @@ export class AddItemComponent implements OnInit {
   @ViewChild('container', { read: ViewContainerRef })
   container!: ViewContainerRef;
 
+  @ViewChild('container', { read: ViewContainerRef })
+  container!: ViewContainerRef;
+
   components: ComponentRef<any>[] = [];
 
   listOfNameCategories: string[] = [];
@@ -56,7 +59,6 @@ export class AddItemComponent implements OnInit {
     const component = this.container.createComponent(componentFactory);
     this.components.push(component);
     this.clicked = true;
-    console.log(this.model.name + "  " + this.model.description + "*******");
   }
 
   /**
