277219cd97dee777075c3abda293b70f341e65f2
Emina <epalalic1@etf.unsa.ba>
2022-12-27 12:37:00 +0100

    Allow the payment on the Stripe

--- Backend/src/main/java/com/developer/auctionapp/dto/request/PaymentRequest.java
+++ Backend/src/main/java/com/developer/auctionapp/dto/request/PaymentRequest.java
@@ -18,11 +18,19 @@ public class PaymentRequest {
     private long productId;
 
     public PaymentRequest() {
+<<<<<<< HEAD
         currency = "";
         description = "";
         amount = 0;
         stripeEmail = "";
         stripeToken = "";
+=======
+        currency = null;
+        description = null;
+        amount = 0;
+        stripeEmail = null;
+        stripeToken = null;
+>>>>>>> 3c6c8490 (Allow the payment on the Stripe)
     }
 
     public PaymentRequest(
