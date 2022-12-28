304b3d3374ae91dda758bfc400207f9fa7eca4f5
Emina <epalalic1@etf.unsa.ba>
2022-12-28 10:14:49 +0100

    Implement adding products to the database and adding images to Firebase

--- Frontend/src/app/core/components/add-item/add-item.component.ts
+++ Frontend/src/app/core/components/add-item/add-item.component.ts
@@ -13,9 +13,6 @@ import { ShippingComponent } from '../shipping/shipping.component';
 
 export class AddItemComponent implements OnInit {
 
-  @ViewChild('container', { read: ViewContainerRef })
-  container!: ViewContainerRef;
-
   components: ComponentRef<any>[] = [];
 
   listOfNameCategories: string[] = [];
