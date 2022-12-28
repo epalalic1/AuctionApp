08c625c14974107883250d37ef1e362186bd7b68
Emina <epalalic1@etf.unsa.ba>
2022-12-28 10:14:49 +0100

    Implement adding products to the database and adding images to Firebase

--- Frontend/src/app/core/services/api.service.ts
+++ Frontend/src/app/core/services/api.service.ts
@@ -60,7 +60,7 @@ export class ApiService {
   addressOfCurrentUser = this.firstPartOfUrl + this.portUrl + 'auctionapp/address/getAddressOfCurrentUser';
   addProduct = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/addItemRequest';
   getBidders = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/getBiddersForProduct';
-  getProductFromId = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/getProductFromId';
+  getProductFromId = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/getProductFromId';)
 
 
   constructor(private http: HttpClient) { }
@@ -148,6 +148,10 @@ export class ApiService {
 
   getAddressOfCurrentUser(): Observable<{ address: Address }> {
     return this.http.get<{  address: Address}>(this.addressOfCurrentUser,{ 'headers': this.loggedInHeaders, responseType: 'json' });
+  }
+
+  addNewProduct(addItem:AddItem): Observable<{ response:Response }> {
+    return this.http.post<{ response:Response }>(this.addProduct, addItem,{ 'headers': this.loggedInHeaders});
   }
 }
 
