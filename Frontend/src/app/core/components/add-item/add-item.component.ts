a41f6c4a9f248d75178c5d34d4d7b9810f693e5e
Emina <epalalic1@etf.unsa.ba>
2022-12-28 10:14:49 +0100

    Implement adding products to the database and adding images to Firebase

--- Frontend/src/app/core/components/add-item/add-item.component.ts
+++ Frontend/src/app/core/components/add-item/add-item.component.ts
@@ -55,6 +55,7 @@ export class AddItemComponent implements OnInit {
     const component = this.container.createComponent(componentFactory);
     this.components.push(component);
     this.clicked = true;
+    console.log(this.model.name + "  " + this.model.description + "*******");
   }
 
   /**
