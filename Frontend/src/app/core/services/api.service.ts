024cb777727bb1898aaef28948773c3d80a374c5
Emina <epalalic1@etf.unsa.ba>
2022-12-29 16:40:58 +0100

    Add improvement

--- Frontend/src/app/core/services/api.service.ts
+++ Frontend/src/app/core/services/api.service.ts
@@ -61,7 +61,6 @@ export class ApiService {
   addProduct = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/addItemRequest';
   getBidders = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/getBiddersForProduct';
   getProductFromId = this.firstPartOfUrl + this.portUrl + 'auctionapp/product/getProductFromId';
-)
 
 
   constructor(private http: HttpClient) { }
@@ -175,6 +174,16 @@ export class ApiService {
 
   addNewProduct(addItem:AddItem): Observable<{ response:Response }> {
     return this.http.post<{ response:Response }>(this.addProduct, addItem,{ 'headers': this.loggedInHeaders});
+  }
+
+  getBiddersForProduct(id:number): Observable<{ bidders: BidderForProduct }> {
+    let params = new HttpParams().set("paramName",id);
+    return this.http.get<{  bidders: BidderForProduct}>(this.getBidders,{ 'headers': this.loggedInHeaders, params: params, responseType: 'json' });
+  }
+
+  getProductById(id:number): Observable<{ product:Product }> {
+    let params = new HttpParams().set("id",id);
+    return this.http.get<{  product:Product }>(this.getProductFromId,{ 'headers': this.headers, params: params, responseType: 'json' });
   }
 }
 
