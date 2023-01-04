6c8d8fb49d14475b1d5ddacc27ebe0b2d8d42bed
Emina <epalalic1@etf.unsa.ba>
2023-01-04 10:28:12 +0100

    Improved the already existing ability to filter products based on category

--- Frontend/src/app/core/components/subcategory/subcategory.component.ts
+++ Frontend/src/app/core/components/subcategory/subcategory.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Product } from '../../models/product';
+import { Subcategory } from '../../models/subcategory';
 
 @Component({
   selector: 'app-subcategory',
@@ -7,14 +10,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SubcategoryComponent implements OnInit {
 
-  constructor() { }
   subcategories = new Set<String>();
 
   @Input()
-  allCategories!: string[];
+  allSubcategories!: string[];
+
+  numberOfProducts: number[] = [];
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.findProductsForEachSubcategory();
   }
+
   toggleEditable(event: any, subcategory: string) {
     if (event.target.checked) {
       this.subcategories.add(subcategory);
@@ -22,5 +30,23 @@ export class SubcategoryComponent implements OnInit {
     else {
       this.subcategories.delete(subcategory);
     }
+  }
+
+  /**
+   * The method we use to find the number of products for each of the subcategories
+   */
+
+  findProductsForEachSubcategory() {
+    this.apiService.getAllProducts().subscribe((allProducts) => {
+      let listOfAllProducts = <Product[]>JSON.parse(JSON.stringify(allProducts));
+      this.apiService.getAllSubcategories().subscribe((allSubcategories) => {
+        let listOfAllSubcategories = <Subcategory[]>JSON.parse(JSON.stringify(allSubcategories));
+        this.allSubcategories.map((nameOfSubcategory) => {
+          let subcategory = listOfAllSubcategories.find(subcategory => subcategory.name === nameOfSubcategory);
+          let productOfSubcategory = listOfAllProducts.filter((product) => product.subcategoryId == subcategory?.id);
+          this.numberOfProducts.push(productOfSubcategory.length);
+        })
+      })
+    })
   }
 }
