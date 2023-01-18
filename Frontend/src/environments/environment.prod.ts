1eea5c57b40e910010814a10bbc9d541ec7b0456
Emina <epalalic1@etf.unsa.ba>
2023-01-18 15:44:26 +0100

    Changes

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true
+    production: true,
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID,
+    api_key: process.env.STRIPE_AK,
+    key: process.env.LOCALSTORAGEKEY
 };
