ba111b9db5c1223a5cc88e554227627fa01de16b
Emina <epalalic1@etf.unsa.ba>
2023-01-21 21:26:04 +0100

    Changes

--- Frontend/src/app/core/components/new-arrivals/new-arrivals.component.ts
+++ Frontend/src/app/core/components/new-arrivals/new-arrivals.component.ts
@@ -2,10 +2,7 @@ import { Component, Injectable, Input, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductImages } from '../../models/product-images';
 import { ApiService } from '../../services/api.service';
-<<<<<<< HEAD
 import { ProductUtils } from '../../utils/product-utils';
-=======
->>>>>>> ec256706 (Add the ability to change the preview of the product as well as change the location where images are retrieved from Firebase)
 import { ItemComponent } from '../item/item.component';
 
 @Component({
@@ -33,33 +30,9 @@ export class NewArrivalsComponent implements OnInit {
     this.apiServis.getNewArrivalsProduct().subscribe((rez) => {
       let products = <Product[]>JSON.parse(JSON.stringify(rez));
       this.newArrivals = products.filter(item => item.status.toString() == 'false');
-<<<<<<< HEAD
-<<<<<<< HEAD
       setTimeout(() => {
-        this.newArrivals = ProductUtils.productsWithListOfImages(this.newArrivals,this.listOfProductsImages)
-      }, 1000);
-=======
->>>>>>> 3c6c8490 (Allow the payment on the Stripe)
-    })
-  }
-
-=======
-      setTimeout(() => {
-        this.newArrivals = this.getImagesOfProduct(this.newArrivals);
+        this.newArrivals = ProductUtils.productsWithListOfImages(this.newArrivals, this.listOfProductsImages)
       }, 1000);
     })
   }
-
-  getImagesOfProduct(newArr: Product[]) {
-    let products =newArr.map((product: Product) => {
-      let listOfProductImag = this.listOfProductsImages.filter((item) => item.productId == product.id);
-      product.imageName.splice(0);
-      listOfProductImag.map((productImg: any) => {
-        product.imageName.push(productImg.images);
-      })
-      return product;
-    });
-    return products;
-  }
->>>>>>> ec256706 (Add the ability to change the preview of the product as well as change the location where images are retrieved from Firebase)
 }
