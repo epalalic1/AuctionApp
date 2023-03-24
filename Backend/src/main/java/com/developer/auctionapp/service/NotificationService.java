ea4adad3577e687360ea264911fb1d02d644ce85
Emina <epalalic1@etf.unsa.ba>
2023-03-24 13:28:55 +0100

    Minor changes

---
+++ Backend/src/main/java/com/developer/auctionapp/service/NotificationService.java
@@ -0,0 +1,13 @@
+package com.developer.auctionapp.service;
+
+import com.developer.auctionapp.dto.request.NotificationRequest;
+import org.springframework.http.ResponseEntity;
+
+public interface NotificationService {
+
+    ResponseEntity<Object> sendNotificationWhenUserIsOutbided(NotificationRequest notificationRequest);
+
+    ResponseEntity<Object> sendNotificationWhenAuctionIsFinished(String message);
+
+    ResponseEntity<Object> getNotificationsByUserId(long id);
+}
